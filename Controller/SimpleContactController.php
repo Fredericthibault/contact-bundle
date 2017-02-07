4054e8a8900ff325f0e2ead798b6c2bda9ae507c
pmdc <pmdavidcaron@gmail.com>
2017-02-07 12:02:05 -0500

    Fix tests and other contact bundle related shits

--- Controller/SimpleContactController.php
+++ Controller/SimpleContactController.php
@@ -17,26 +17,67 @@ class SimpleContactController extends Controller
 {
     public function sendSimpleFormAction(Request $request)
     {
-        $name = $request->request->get('name');
-        $mail = $request->request->get('email');
-        $mess = $request->request->get('message');
-        $sendTo = $this->getParameter('viweb_contact.main_mail');
-        $message = \Swift_Message::newInstance()
-            ->setSubject('Nouveau contact')
-            ->setFrom('send@example.com')
-            ->setTo($sendTo)
-            ->setBody(
-                $this->renderView('@ViwebContact/SimpleContact/mail.html.twig', [
-                    'nom' => $name,
-                    'mail' => $mail,
-                    'message' => $mess
-                ]),
-                'text/html'
-            )
-        ;
-        $this->get('mailer')->send($message);
+        if($request->request->get('l_name')){
+            $l_name = $request->request->get('l_name');
+            $f_name = $request->request->get('f_name');
+            $adress = $request->request->get('adress');
+            $city = $request->request->get('city');
+            $province = $request->request->get('province');
+            $postal = $request->request->get('postal');
+            $mail = $request->request->get('mail');
+            $phone = $request->request->get('phone');
+            $cell = $request->request->get('cell');
+            $reason = $request->request->get('reason');
+            $main_text = $request->request->get('main_text');
 
-        return new JsonResponse(['stauts' => 'ok']);
+            $sendTo = $this->getParameter('viweb_contact.main_mail');
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Nouveau contact')
+                ->setFrom('send@example.com')
+                ->setTo($sendTo)
+                ->setBody(
+                    $this->renderView('@ViwebContact/SimpleContact/longMail.html.twig', [
+                        'lastName' => $l_name,
+                        'firstName' => $f_name,
+                        'mail' => $mail,
+                        'address' => $adress,
+                        'city' => $city,
+                        'province' => $province,
+                        'postal' => $postal,
+                        'phone' => $phone,
+                        'cell' => $cell,
+                        'reason' => $reason,
+                        'message' => $main_text
+                    ]),
+                    'text/html'
+                )
+            ;
+            $this->get('mailer')->send($message);
+
+            return new JsonResponse(['stauts' => 'ok']);
+        } else {
+            $name = $request->request->get('name') ?? 'Non disponible';
+            $mail = $request->request->get('email');
+            $mess = $request->request->get('message');
+            $sendTo = $this->getParameter('viweb_contact.main_mail');
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Nouveau contact')
+                ->setFrom('send@example.com')
+                ->setTo($sendTo)
+                ->setBody(
+                    $this->renderView('@ViwebContact/SimpleContact/mail.html.twig', [
+                        'nom' => $name,
+                        'mail' => $mail,
+                        'message' => $mess
+                    ]),
+                    'text/html'
+                )
+            ;
+            $this->get('mailer')->send($message);
+
+            return new JsonResponse(['stauts' => 'ok']);
+        }
+
     }
 
 
